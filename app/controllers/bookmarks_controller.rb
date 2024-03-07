522adb63e4a74e86215925b0b0fea82425dd9901
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-03-07 20:21:31 +0000

    in progress

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,19 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = List.find(params[:list_id])
+    @bookmark.movie = Movie.find(params[:movie_id])
+    @bookmark.save
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
