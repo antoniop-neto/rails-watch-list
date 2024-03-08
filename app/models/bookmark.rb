cd375a583e1fc887f2348809110f2e93e3ea7b37
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-03-08 15:53:43 +0000

    first part done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ApplicationRecord
-  belongs_to :movie, dependent: :destroy
-  belongs_to :list, dependent: :destroy
+  belongs_to :movie
+  belongs_to :list
 
   validates :movie, uniqueness: { scope: :list }
   validates :comment, length: { minimum: 6 }
